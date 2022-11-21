8f2070c019bd7371e9fcaf0d5baa01d510aec1eb
Subhrajyoti <sjs14031994@gmail.com>
2022-11-21 21:35:14 +0530

    Added changes to driver

--- go/samples/http/mysqldb/mysql_driver.go
+++ go/samples/http/mysqldb/mysql_driver.go
@@ -2,22 +2,22 @@ package mysqldb
 
 import (
 	"log"
+	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/google/sqlcommenter/go/core"
 	gosql "github.com/google/sqlcommenter/go/database/sql"
 )
 
-func ConnectMySQL(connection string) *gosql.DB {
-	db, err := gosql.Open("mysql", connection, core.CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableAction: true, EnableFramework: true, EnableTraceparent: true})
+func ConnectMySQL(connection string) *sql.DB {
+	db, err := gosql.Open("mysql", connection)
 	if err != nil {
 		log.Fatalf("Failed to connect to MySQL(%q), error: %v", connection, err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Failed to ping the database, error: %v", err)
-	}
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Fatalf("Failed to ping the database, error: %v", err)
+	// }
 
 	return db
 }
--- go/samples/http/pgdb/pg_driver.go
+++ go/samples/http/pgdb/pg_driver.go
@@ -2,22 +2,22 @@ package pgdb
 
 import (
 	"log"
+	"database/sql"
 
-	"github.com/google/sqlcommenter/go/core"
 	gosql "github.com/google/sqlcommenter/go/database/sql"
 	_ "github.com/lib/pq"
 )
 
-func ConnectPG(connection string) *gosql.DB {
-	db, err := gosql.Open("postgres", connection, core.CommenterOptions{EnableDBDriver: true, EnableRoute: true, EnableAction: true, EnableFramework: true, EnableTraceparent: true})
+func ConnectPG(connection string) *sql.DB {
+	db, err := gosql.Open("postgres", connection)
 	if err != nil {
 		log.Fatalf("Failed to connect to PG(%q), error: %v", connection, err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Failed to ping the database, error: %v", err)
-	}
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Fatalf("Failed to ping the database, error: %v", err)
+	// }
 
 	return db
 }
